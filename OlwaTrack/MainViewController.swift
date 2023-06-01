1149c717fbf67f0c1be2186ef24d972a8a2bae1c
Желанов Александр Валентинович <alexandrzhelanov@gmail.com>
2023-06-01 04:12:25 +0300

    WAYS-2 Создать проект

---
+++ OlwaTrack/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  OlwaTrack
+//
+//  Created by Желанов Александр Валентинович on 01.06.2023.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
